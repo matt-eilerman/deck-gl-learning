a82696155ddf70a8fac5c3273f58a4ec2e134b2e
Matt Eilerman <matt.eilerman@theperducogroup.com>
2021-08-12 13:57:56 -0400

    add 3d hexbin example, update dependencies

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App, { DATA_URL } from './App';
 import reportWebVitals from './reportWebVitals';
+
+const root = document.getElementById('root');
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  root
 );
+
+require('d3-request').csv(DATA_URL, (error, response) => {
+  if (!error) {
+    const data = response.map(d => [Number(d.lng), Number(d.lat)]);
+    ReactDOM.render(<App data={data} />, root);
+  }
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
