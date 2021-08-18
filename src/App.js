913ae20c0c1360f1a43833756fac0c60d3b66d2e
Matt Eilerman <matt.eilerman@theperducogroup.com>
2021-08-18 12:28:59 -0400

    prevent deck.gl rendering but retrieve underlying hexbin data for use with cesium

--- src/App.js
+++ src/App.js
@@ -1,114 +1,59 @@
-import React from 'react';
-import { StaticMap } from 'react-map-gl';
-import { AmbientLight, PointLight, LightingEffect } from '@deck.gl/core';
+import React, { useState, useRef, useEffect } from 'react';
 import { HexagonLayer } from '@deck.gl/aggregation-layers';
 import DeckGL from '@deck.gl/react';
-// import './App.css';
+import './App.css';
 
 export const DATA_URL = 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv';
 
 const App = ({ data }) => {
 
-  const ambientLight = new AmbientLight({
-    color: [255, 255, 255],
-    intensity: 1.0
-  });
-
-  const pointLight1 = new PointLight({
-    color: [255, 255, 255],
-    intensity: 0.8,
-    position: [-0.144528, 49.739968, 80000]
-  });
-
-  const pointLight2 = new PointLight({
-    color: [255, 255, 255],
-    intensity: 0.8,
-    position: [-3.807751, 54.104682, 8000]
-  });
-
-  const lightingEffect = new LightingEffect({ ambientLight, pointLight1, pointLight2 });
-
-  const material = {
-    ambient: 0.64,
-    diffuse: 0.6,
-    shininess: 32,
-    specularColor: [51, 51, 51]
-  };
-
-  const INITIAL_VIEW_STATE = {
-    longitude: -1.415727,
-    latitude: 52.232395,
-    zoom: 6.6,
-    minZoom: 5,
-    maxZoom: 15,
-    pitch: 40.5,
-    bearing: -27
-  };
-
-  const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json';
-  const ACCESS_TOKEN = 'pk.eyJ1IjoibWF0dC1laWxlcm1hbiIsImEiOiJja3M5NXQxOGMwcmg1MzFwZW9vcTY4aHljIn0.NDhWB4CYmJR7KURZyoQb-g';
-
-  const colorRange = [
-    [1, 152, 189],
-    [73, 227, 206],
-    [216, 254, 181],
-    [254, 237, 177],
-    [254, 173, 84],
-    [209, 55, 78]
-  ];
-
-  const getTooltip = ({ object }) => {
-    if (!object) {
-      return null;
-    }
-    const lat = object.position[1];
-    const lng = object.position[0];
-    const count = object.points.length;
-
-    return `\
-      latitude: ${Number.isFinite(lat) ? lat.toFixed(6) : ''}
-      longitude: ${Number.isFinite(lng) ? lng.toFixed(6) : ''}
-      ${count} Accidents`;
-  }
+  const [subLayer, setSubLayer] = useState();
+  const [binData, setBinData] = useState();
+  const interval = useRef();
 
   const layers = ([
     new HexagonLayer({
       id: 'heatmap',
-      colorRange,
-      coverage: 1,
       data,
       elevationRange: [0, 3000],
       elevationScale: data && data.length ? 50 : 0,
       extruded: true,
       getPosition: d => d,
-      
-      pickable: true,
       radius: 1000,
-      upperPercentile: 100,
-      material,
-
-      transitions: {
-        elevationScale: 3000
-      }
+      upperPercentile: 100
     })
   ]);
 
-  setTimeout(() => {
-    let subLayer = layers[0].getSubLayers()[0];
-    if (subLayer) console.log(subLayer.props.data.map(bin => ({ points: bin.points.map(pt => pt.source), position: bin.position })));
-  }, 500);
+  const getSubLayer = () => {
+    const sl = layers[0].getSubLayers()[0];
+    if (sl) setSubLayer(sl);
+  };
+
+  useEffect(() => {
+    interval.current = setInterval(getSubLayer, 100);
+    return () => clearInterval(interval.current);
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    if (subLayer) {
+      setBinData(subLayer.props.data.map(bin => ({ points: bin.points.map(pt => pt.source), position: bin.position })));
+    }
+  }, [subLayer]);
+
+  useEffect(() => {
+    if (binData && binData.length) {
+      console.log(binData);
+      clearInterval(interval.current);
+    }
+  }, [binData]);
 
   return (
     <div className='app'>
       <DeckGL
         layers={layers}
-        effects={[lightingEffect]}
-        initialViewState={INITIAL_VIEW_STATE}
-        controller={true}
-        getTooltip={getTooltip}
-      >
-        <StaticMap reuseMaps mapboxApiAccessToken={ACCESS_TOKEN} mapStyle={MAP_STYLE} preventStyleDiffing={true} />
-      </DeckGL>
+        initialViewState={{}}
+      />
     </div>
   );
 }
